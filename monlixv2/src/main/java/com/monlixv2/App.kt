d529a34f2a1b65921d62df16f93598ef6bfb58bf
filip vujovic <cofilicavujovic@gmail.com>
2022-04-18 15:58:45 +0200

    Remove multidex

--- monlixv2/src/main/java/com/monlixv2/App.kt
+++ monlixv2/src/main/java/com/monlixv2/App.kt
@@ -1,8 +1,7 @@
 package com.monlixv2
 
+import android.app.Application
 import android.content.Context
-import androidx.multidex.MultiDex
-import androidx.multidex.MultiDexApplication
 import com.monlixv2.service.models.ads.AdRepository
 import com.monlixv2.service.localDb.OfferDatabase
 import com.monlixv2.service.models.campaigns.CampaignRepository
@@ -11,17 +10,15 @@ import com.monlixv2.service.models.surveys.SurveyRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
-class App : MultiDexApplication() {
+class App : Application() {
     private val applicationScope = CoroutineScope(SupervisorJob())
 
     private val database by lazy { OfferDatabase.getDatabase(this, applicationScope) }
-    val repository by lazy { OfferRepository(database.offerDao()) }
     val adsRepository by lazy { AdRepository(database.adDao()) }
     val surveyRepository by lazy { SurveyRepository(database.surveyDao()) }
     val campaignsRepository by lazy { CampaignRepository(database.campaignDao()) }
 
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
-        MultiDex.install(this)
     }
 }
